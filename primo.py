0c7ca0d2b5f4247fb82a9540d65ff9bc381dc21f
Cleitonjorgedasilva <cleiton.silva@aluno.faculdadeimpacta.com.br>
2020-10-23 02:41:07 -0300

    naluta

--- primo.py
+++ primo.py
@@ -5,22 +5,21 @@ from math import sqrt
 app = Flask(__name__)
 @app.route('/')
 def primos():
-    def nao_entre_em_panico():
     limite =  100
 
     c = 1
-    p = 1
-    numero = 3
-    primos = "2, "
-    while p < limite:
+    a = 1
+    numero = 2
+    primos = "0, "
+    while a < limite:
         ehprimo = 1
-        for i in range(2, numero):
+        for i in range(1, numero):
             if numero%i == 0:
                 ehprimo = 0
                 break
         if (ehprimo):
             primos = primos + str(numero) + ","
-        p+=1
+            a+=1
         numero +=1
 
     return primos
