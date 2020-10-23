8d3514d5b342078e11d65076dd521f8b766b11e1
Cleitonjorgedasilva <cleiton.silva@aluno.faculdadeimpacta.com.br>
2020-10-23 01:36:37 -0300

    ac3

--- primos.py
+++ primos.py
@@ -15,8 +15,7 @@ def primos():
             primos.append(cont)
         cont +=1
     return primos
-print (primos())
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
