1cd6144498245314da71c4b371d37c80f37ec838
Cleitonjorgedasilva <cleiton.silva@aluno.faculdadeimpacta.com.br>
2020-10-23 02:49:48 -0300

    teste

--- primo.py
+++ primo.py
@@ -1,17 +1,9 @@
-import os
-from flask import Flask, jsonify, request
-from math import sqrt
-
-app = Flask(__name__)
-@app.route('/')
-def primos():
-    limite =  100
 
     c = 1
     p = 1
     numero = 3
     primos = "2, "
-    while a < limite:
+    while p < limite:
         ehprimo = 1
         for i in range(2, numero):
             if numero%i == 0:
@@ -27,4 +19,7 @@ def primos():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-    
+
+primo.py[+] [unix] (02:45 23/10/2020)                                                       22,1 All
+-- INSERT --
+ 
