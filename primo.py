52ef981625f4542fbcb156f754d5c912c672483e
Cleitonjorgedasilva <cleiton.silva@aluno.faculdadeimpacta.com.br>
2020-10-26 21:34:31 -0300

    AC3

--- primo.py
+++ primo.py
@@ -1,5 +1,12 @@
 
-    c = 1
+import os
+from flask import Flask, jnofiny, request
+from math import sqrt
+
+app = Flask(__name__)
+@app.route('/')
+
+def primos():
     p = 1
     numero = 3
     primos = "2, "
@@ -13,15 +20,10 @@
             primos = primos + str(numero) + ","
             p+=1
         numero +=1
-        if(p%10 == 0):
-            primos = primos + '<br>'
 
     return primos
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-
-primo.py[+] [unix] (02:45 23/10/2020)                                                       22,1 All
--- INSERT --
  
